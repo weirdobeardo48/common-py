7e0a03c6bbe6e044e55f8999771048f95f3fba0d
TruongNX <truongnx@easyshoppingfeed.com>
2021-11-15 17:56:54 +0700

    Hu

--- common_py/logger.py
+++ common_py/logger.py
@@ -9,18 +9,39 @@ class LoggerUtils():
 
     def init_log(self):
         # Just a workaround, but whatever, this is just a cheap script
-        self.LOG_FILE_PATH = os.getenv('COMMON_PY_LOG_FILE_PATH','./')
+        self.LOG_FILE_PATH = os.getenv('COMMON_PY_LOG_FILE_PATH', './')
         self.LOG_FILE_LEVEL = os.getenv('COMMON_PY_LOG_FILE_LEVEL', 'WARN')
         self.LOG_STDOUT_LEVEL = os.getenv('COMMON_PY_STDOUT_LOG_LEVEL', 'WARN')
-        self.LOG_DEFAULT_LEVEL = os.getenv('COMMON_PY_DEFAULT_LOG_LEVEL', 'WARN')
+        self.LOG_DEFAULT_LEVEL = os.getenv(
+            'COMMON_PY_DEFAULT_LOG_LEVEL', 'WARN')
         self.LOG_FILE_ENABLED = os.getenv('COMMON_PY_ENABLE_LOG_FILE', '0')
         handlers = []
+        handler = {}
+
+        handler['stdout_handler'] = {
+            'class': 'logging.StreamHandler',
+            'level': self.LOG_STDOUT_LEVEL,
+            'formatter': 'standard'
+        }
+
         handlers.append('stdout_handler')
         if self.LOG_FILE_ENABLED == '1':
             handlers.append('file_handler')
             if os.path.join(self.LOG_FILE_PATH):
                 if not os.path.exists(os.path.join(self.LOG_FILE_PATH)):
                     os.makedirs(os.path.join(self.LOG_FILE_PATH))
+
+            handler['file_handler'] = {
+                'class': 'logging.handlers.TimedRotatingFileHandler',
+                'level': self.LOG_FILE_LEVEL,
+                'formatter': 'standard',
+                'filename': os.path.join(self.LOG_FILE_PATH, 'application.log'),
+                'encoding': 'utf8',
+                'backupCount': 10,
+                'when': 'd',
+                'interval': 1,
+            }
+
         logging_config = {
             'version': 1,
             'disable_existing_loggers': False,
@@ -29,23 +50,7 @@ class LoggerUtils():
                     'format': '%(asctime)s [%(levelname)s] %(name)s [%(processName)s %(threadName)s]: %(lineno)d: %(message)s'
                 },
             },
-            'handlers': {
-                'file_handler': {
-                    'class': 'logging.handlers.TimedRotatingFileHandler',
-                    'level': self.LOG_FILE_LEVEL,
-                    'formatter': 'standard',
-                    'filename': os.path.join(self.LOG_FILE_PATH, 'application.log'),
-                    'encoding': 'utf8',
-                    'backupCount': 10,
-                    'when': 'd',
-                    'interval': 1,
-                },
-                'stdout_handler': {
-                    'class': 'logging.StreamHandler',
-                    'level': self.LOG_STDOUT_LEVEL,
-                    'formatter': 'standard'
-                }
-            },
+            'handlers': handler,
             'loggers': {
                 '': {
                     'handlers': handlers,
