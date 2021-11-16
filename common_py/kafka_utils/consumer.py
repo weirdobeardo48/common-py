2e9ce567c2456d67b75806fa2966ed73d37a8ba4
TruongNX <truongnx@easyshoppingfeed.com>
2021-11-16 10:33:56 +0700

    Hm

--- common_py/kafka_utils/consumer.py
+++ common_py/kafka_utils/consumer.py
@@ -50,7 +50,7 @@ class Consumer():
                         if value:
                             LOG.warning(
                                 f'Seeking offset of topic {partition.topic} partition {partition.partition} to offset {value}')
-                            consumer.seek(partition.topic, value)
+                            consumer.seek(partition, value)
                         pass
 
             def on_partitions_revoked(self, revoked):
